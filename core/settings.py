cfdd14d5eca10d1fdba19b9a8beac71b64d13951
myra2004 <amirovamuqaddas@gmail.com>
2025-06-28 21:00:20 +0500

    transtlation again

--- core/settings.py
+++ core/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
